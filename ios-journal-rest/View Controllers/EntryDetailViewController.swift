fcfbc1d6f037962885607c781cf63f644711ec04
Conner Alegre <cga0@pm.me>
2018-08-09 11:56:52 -0600

    add EntryDetailViewController outlets

---
+++ ios-journal-rest/View Controllers/EntryDetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  EntryDetailViewController.swift
+//  ios-journal-rest
+//
+//  Created by Conner on 8/9/18.
+//  Copyright © 2018 Conner. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func saveJournalEntry(_ sender: Any) {
+    }
+    
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var textView: UITextView!
+}
